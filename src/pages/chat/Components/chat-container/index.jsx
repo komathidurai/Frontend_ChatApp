2ce43cf68a53cd76d378b98b18f7c23c534b2dd3
Vibinraj-unique <vibin170315@gmail.com>
2025-02-16 10:31:41 +0530

    uncommited changes

--- src/pages/chat/Components/chat-container/index.jsx
+++ src/pages/chat/Components/chat-container/index.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const ChatContainer = () => {
   return (
-    <div> ChatContainer</div>
+    <div className='fixed top-0 h-[100vh] w-[100vw] bg-[#1c1d25] flex flex-col md:static md:flex-1'> ChatContainer
+    
+    </div>
   )
 }
 
--- src/pages/chat/index.jsx
+++ src/pages/chat/index.jsx
@@ -1,10 +1,12 @@
 import { useAppStore } from '@/store';
 import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
-// import  toast  from 'react-toastify';
+import  toast  from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import  toast  from 'react-hot-toast';
-
+import ChatContainer from './Components/chat-container';
+import ContactsContainer from './Components/contact-container';
+import EmptyChatContainer from './Components/empty-chat-container';
 
 
 const Chat = () => {
@@ -17,8 +19,14 @@ const Chat = () => {
     }
  },[userInfo, navigate]);
 
-  return <div>chat</div>
-  
+  return (
+  <div className='flex h-[100vh] text-white overflow-hidden'>chat
+  <ContactsContainer />
+  <ChatContainer />
+  <EmptyChatContainer />
+  </div>
+
+); 
 };
 
 export default Chat;
